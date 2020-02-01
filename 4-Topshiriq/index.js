4ffb9d3b1c2ca5d6161c1462b204297f22e35061
Farkhod Dadajanov <dfarkhod@gmail.com>
2020-02-01 16:24:29 +0800

    Added files for the new exercise

---
+++ 4-Topshiriq/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const categoriesRoute = require('./routes/categories');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/virtualdars', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDBga ulanish hosil qilindi...');
+  })
+  .catch((err) => {
+    console.error('MongoDBga ulanish vaqtida xato ro\'y berdi...', err);
+  });
+app.use(express.json());
+app.use('/api/categories', categoriesRoute);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`${port}chi portni eshitishni boshladim...`);
+});
