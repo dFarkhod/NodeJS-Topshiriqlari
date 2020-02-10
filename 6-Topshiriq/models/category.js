9b97e32859cebb809b48c3ddb2f123dd6d24c526
Farkhod Dadajanov <dfarkhod@gmail.com>
2020-02-10 21:29:46 +0800

    added new assignment codes

---
+++ 6-Topshiriq/models/category.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+});
+
+
+function validateCategory(category) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(category, schema);
+}
+
+
+const Category = mongoose.model('Category', categorySchema);
+
+exports.Category = Category;
+exports.validate = validateCategory;
