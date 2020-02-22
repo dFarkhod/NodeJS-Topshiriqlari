4c1611632266951c516f688e8996bb311173f8a2
Farkhod Dadajanov <dfarkhod@gmail.com>
2020-02-22 13:17:24 +0800

    added assignment #7

---
+++ 7-Topshiriq/practice.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/virtualdars2', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDBga ulanish hosil qilindi...'))
+  .catch((err) => console.error('MongoDBga ulanish vaqtida xato ro\'y berdi...', err));
+
+const Author = mongoose.model('Author', new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String
+}));
+
+const Book = mongoose.model('Book', new mongoose.Schema({
+  title: String
+}));
+
+async function createAuthor(firstName, lastName, email) {
+  const author = new Author({
+    firstName,
+    lastName,
+    email
+  });
+
+  const result = await author.save();
+  console.log(result);
+}
+
+async function createBook(title, author) {
+  const book = new Book({
+    title: title
+  });
+
+  const result = await book.save();
+  console.log(result);
+}
+
+async function listBooks() {
+  const book = await Book
+    .find()
+    .select('title author');
+  console.log(book);
+}
+
+//createAuthor('Farkhod', 'Dadajanov', 'dfarkhod@gmail.com');
+
+//createBook('NodeJS - To\'liq qo\'llanma', 'authorId')
+
+listBooks();
