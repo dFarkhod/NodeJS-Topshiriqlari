438d7d157f7f1d21b0b1384be2809b060dd8abf6
Farkhod Dadajanov <dfarkhod@gmail.com>
2020-02-29 20:06:05 +0800

    added comments

--- 8-Topshiriq/init.js
+++ 8-Topshiriq/init.js
@@ -3,6 +3,8 @@ const User = require('./models/user');
 
 const connString = 'mongodb://FarkhodPC:27019/trans';
 async function initDatabase() {
+
+  // bazaga ulanamiz
   await mongoose.connect(connString, {
     replicaSet: 'rs',
     useNewUrlParser: true,
@@ -12,7 +14,10 @@ async function initDatabase() {
   const senderAccountNumber = 'SA1002001';
   const receiverAccountNumber = 'SA3104215';
 
+  // jo'natuvchini hisob raqami bo'yicha bazadan izlab ko'ramiz
   const sender = await User.findOne({ accountNumber: senderAccountNumber });
+
+  // agar topilmasa, unda bazaga yangi hujjat qo'shamiz
   if (!sender) {
     const sender = new User({
       accountNumber: senderAccountNumber, name: 'Ahmad', balance: 50000.00
@@ -20,7 +25,10 @@ async function initDatabase() {
     await sender.save();
   }
 
+  // oluvchini bazadan hisob raqami bo'yicha izlab ko'ramiz
   const receiver = await User.findOne({ accountNumber: receiverAccountNumber });
+
+  // agar topilmasa, bazaga yangi hujjat qo'shamiz
   if (!receiver) {
     receiver = new User({
       accountNumber: receiverAccountNumber, name: 'Anvar', balance: 1200.00
@@ -28,4 +36,6 @@ async function initDatabase() {
     await receiver.save();
   }
 }
+
+// initDatabase funktsiyasini modulimizda eksport qilamiz
 module.exports = initDatabase;
--- 8-Topshiriq/tran.js
+++ 8-Topshiriq/tran.js
@@ -5,63 +5,71 @@ const User = require('./models/user');
 const Journal = require('./models/journal');
 
 async function transferMoney(senderAccountNumber, receiverAccountNumber, amount) {
-    // connect the DB and get the User Model
+    // ma'lumotlar omboriga ulanib olib, unda bizga kerakli bo'lgan
+    // boshlang'ich ma'lumot yozib qo'yamiz
     await initDatabase();
+
+    // sessiyani boshlaymiz
     const session = await mongoose.startSession();
+
+    // sessiyamiz ichida tranzaktsiya ochamiz
     session.startTransaction();
     try {
-        // always pass session to find queries when the data is needed for the transaction session
+        // jo'natuvchini bazadan izlab topamiz, bunda bazaga so'rov berilyapti, shuning uchun
+        // tranzaktsiya ichida bajariladigan barcha so'rovlarga sessiyani ham
+        // berib yuborishimiz kerak
         const sender = await User.findOne({ accountNumber: senderAccountNumber }).session(session);
         if (!sender)
             throw new Error('Sender not found');
 
-        // calculate the updated sender balance
+        // jo'natuvchining hisobidan tranzaktsiya miqdoricha pulni ayrib tashlaymiz
         sender.balance = currency(sender.balance).subtract(amount);
 
-        // if funds are insufficient, the transfer cannot be processed
+        // agarda uning hisobidagi pul yetarli bo'lmasa, xato qaytaramiz
         if (sender.balance < 0) {
             throw new Error(`User - ${sender.name} has insufficient funds`);
         }
 
-        // save the sender updated balance
-        // do not pass the session here
-        // mongoose uses the associated session here from the find query return
-        // more about the associated session ($session) later on
+        // jo'natuvchiga qilingan o'zgarishlarni bazaga yozib qo'yamiz
+        // bu yerda sessiya obyektini berishni keragi yo'q.
         await sender.save();
 
+        // jo'natuvchining hisobidan pul yechib olingan amal haqida
+        // jurnal yozamiz
         const debitJournal = new Journal({
             accountNumber: sender.accountNumber, operation: 'Debit', amount: amount
         })
         await debitJournal.save();
 
+        // oluvchini bazadan izlab topamiz va agar u topilmasa xato qaytaramiz
         const receiver = await User.findOne({ accountNumber: receiverAccountNumber }).session(session);
         if (!receiver)
             throw new Error('Receiver not found');
 
+        // oluvchining hisobiga tranzaktsiya miqdoricha pulni qo'shamiz
+        // va uni bazaga yozib qo'yamiz
         receiver.balance = currency(receiver.balance).add(amount);
         await receiver.save();
 
+        // oluvchining hisobiga pul qo'shilganlik amalini jurnalga yozib qo'yamiz
         const creditJournal = new Journal({
             accountNumber: receiver.accountNumber, operation: 'Credit', amount: amount
         })
         await creditJournal.save();
 
-        // commit the changes if everything was successful
+        // agarda shu joygacha hammasi muvaffaqqiyatli o'tgan bo'lsa, sessiyani commit qilamiz
         await session.commitTransaction();
         console.log('Transaction has been completed successfully!');
     } catch (error) {
-        // if anything fails above just rollback the changes here
-
-        // this will rollback any changes made in the database
+        // agarda yuqorida try block ichida bironta xato chiqqan bo'lsa
+        // unda barcha qilingan o'zgarishlar bekor qilinadi va 
+        // ma'lumotlar omboriga hech narsa yozilmaydi
         await session.abortTransaction();
 
-        // logging the error
         console.error(error);
-
-        // rethrow the error
         throw error;
     } finally {
-        // ending the session
+        // har qanday xolatda ham ishimiz so'ngida sessiyani yopamiz
         session.endSession();
     }
 }
