1f115e013c1ea1c3f2d9beb8d1998873c77d1a8f
Farkhod Dadajanov <dfarkhod@gmail.com>
2020-03-20 19:32:31 +0800

    added next assignment files

---
+++ 13-Topshiriq/routes/users.js
@@ -0,0 +1,21 @@
+const { User, validate } = require('../models/user');
+const express = require('express');
+const router = express.Router();
+const _ = require('lodash');
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error)
+    return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user)
+    return res.status(400).send('Mavjud bo\'lgan foydalanuvchi');
+
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  await user.save();
+
+  res.send(_.pick(user, ['_id', 'name', 'email']));
+});
+
+module.exports = router; 
