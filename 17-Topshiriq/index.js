9c117f4c1e76250d292370ee07e13165cff54f2f
Farkhod Dadajanov <dfarkhod@gmail.com>
2020-04-04 11:38:28 +0800

    next assignment

---
+++ 17-Topshiriq/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const categoriesRoute = require('./routes/categories');
+const customersRoute = require('./routes/customers');
+const coursesRoute = require('./routes/courses');
+const entrollmentsRoute = require('./routes/enrollments');
+const usersRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('config');
+
+if (!config.get('jwtPrivateKey')) {
+  console.error('JIDDIY XATO: virtualdars_jwtPrivateKey muhit o\'zgaruvchisi aniqlanmagan.');
+  process.exit(1);
+}
+
+mongoose.connect('mongodb://localhost/virtualdars', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDBga ulanish hosil qilindi...');
+  })
+  .catch((err) => {
+    console.error('MongoDBga ulanish vaqtida xato ro\'y berdi...', err);
+  });
+mongoose.set('useFindAndModify', false);
+app.use(express.json());
+app.use('/api/categories', categoriesRoute);
+app.use('/api/customers', customersRoute);
+app.use('/api/courses', coursesRoute);
+app.use('/api/enrollments', entrollmentsRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/auth', authRoute);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`${port}chi portni eshitishni boshladim...`);
+});
---
+++ 17-Topshiriq/models/user.js
@@ -0,0 +1,46 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  }
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+  return token;
+}
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
