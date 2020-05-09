4e8c3737c6c818c4eccdb436763122a1a893a31c
Farkhod Dadajanov <dfarkhod@gmail.com>
2020-05-09 10:49:18 +0800

    Updates

--- 22-Topshiriq/mylib.js
+++ 22-Topshiriq/mylib.js
@@ -1,6 +1,4 @@
 // Sonlarni test qilish - mutlaq qiymat
 module.exports.absolute = function (number) {
-  if (number > 0) return number;
-  if (number < 0) return -number;
-  return 0;
+  return (number >= 0) ? number : -number;
 }
--- 22-Topshiriq/tests/mylib.test.js
+++ 22-Topshiriq/tests/mylib.test.js
@@ -1,3 +1,17 @@
-test('Mening ilk testim', () => {
-    throw new Error('bir nima ishlamay qoldi');
-});
+const myLib = require('../mylib');
+describe('absolute', () => {
+    it('should return a positive number if input is positive', () => {
+        const result = myLib.absolute(1);
+        expect(result).toBe(1);
+    });
+
+    it('should return a positive number if input is negative', () => {
+        const result = myLib.absolute(-1);
+        expect(result).toBe(1);
+    });
+
+    it('should return 0 if input is 0', () => {
+        const result = myLib.absolute(0);
+        expect(result).toBe(0);
+    });
+})
