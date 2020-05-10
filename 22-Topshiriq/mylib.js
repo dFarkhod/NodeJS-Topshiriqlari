8a30e1f2ed12bef906ff8b2eda973f063c194929
Farkhod Dadajanov <dfarkhod@gmail.com>
2020-05-10 13:30:09 +0800

    Updates

--- 22-Topshiriq/mylib.js
+++ 22-Topshiriq/mylib.js
@@ -2,3 +2,21 @@
 module.exports.absolute = function (number) {
   return (number >= 0) ? number : -number;
 }
+
+
+// Matnlarni test qilish
+module.exports.salam = function (name) {
+  return 'Assalomu alaykum, ' + name + '!';
+}
+
+
+// Qatorlarni test qilish
+module.exports.getCurrencies = function () {
+  return ['UZS', 'MYR', 'TRY'];
+}
+
+
+// Obyektlarni test qilish
+module.exports.getProduct = function (productId) {
+  return { id: productId, title: 'banana', price: 2 };
+}
--- 22-Topshiriq/tests/mylib.test.js
+++ 22-Topshiriq/tests/mylib.test.js
@@ -14,4 +14,41 @@ describe('absolute', () => {
         const result = myLib.absolute(0);
         expect(result).toBe(0);
     });
-})
+});
+
+
+describe('salam', () => {
+    it('should return the greeting message', () => {
+        const result = myLib.salam('Farhod');
+        //expect(result).toContain('Farhod');
+        expect(result).toMatch(/Farhod/);
+    });
+});
+
+
+describe('getCurrencies', () => {
+    it('should return default currencies', () => {
+        const result = myLib.getCurrencies();
+
+        // o'ta umumiy test
+        expect(result).toBeDefined();
+        expect(result).not.toBeNull();
+
+        // o'ta aniq test
+        expect(result[0]).toBe('UZS');
+        expect(result[1]).toBe('MYR');
+        expect(result[2]).toBe('TRY');
+        expect(result.length).toBe(3);
+
+        // to'g'ri uslubda yozilgan test
+        expect(result).toContain('UZS');
+        expect(result).toContain('TRY');
+        expect(result).toContain('MYR');
+        expect(result).toEqual(expect.arrayContaining(['MYR', 'UZS', 'TRY']));
+
+    });
+});
+
+
+
+
