c0daaefd66cfe2674fa4eb1f23da7a1a0d660ded
Farkhod Dadajanov <dfarkhod@gmail.com>
2020-05-31 15:33:03 +0800

    updated

--- 23-Topshiriq/tests/integration/categories.test.js
+++ 23-Topshiriq/tests/integration/categories.test.js
@@ -1,17 +1,27 @@
 const request = require('supertest');
 let server;
+const { Category } = require('../../models/category');
 
 describe('/api/categores', () => {
     beforeEach(() => {
         server = require('../../index');
     });
-    afterEach(() => {
+    afterEach(async () => {
         server.close();
+        await Category.remove({});
     });
     describe('GET /', () => {
         it('should return all categories', async () => {
+            await Category.collection.insertMany([
+                { name: 'dasturlash' },
+                { name: 'tarmoqlar' },
+                { name: 'dizayn' }
+            ]);
+
             const response = await request(server).get('/api/categories');
             expect(response.status).toBe(200);
+            expect(response.body.length).toBe(3);
+            expect(response.body.some(cat => cat.name == 'dasturlash')).toBeTruthy();
         });
     });
 });
